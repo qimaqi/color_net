c54513a5554ed4e30b33c555ce94e20b6b6b510b
AlexanderNevada <alexander.qi.eins@gmail.com>
2021-05-20 12:41:30 +0200

    resume training

--- train.py
+++ train.py
@@ -20,7 +20,7 @@ original_transform = transforms.Compose([
 ])
 
 have_cuda = torch.cuda.is_available()
-epochs = 32
+epochs = 64
 
 data_dir = '/cluster/scratch/qimaqi/data_5k/colorization/'  # "../images256/"
 train_set = TrainImageFolder(data_dir, original_transform)
@@ -30,8 +30,8 @@ print('train_set_size',train_set_size)
 train_set_classes = train_set.classes
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=32, shuffle=True, num_workers=4)
 color_model = ColorNet()
-if os.path.exists('./colornet_params.pth'):
-    color_model.load_state_dict(torch.load('colornet_params.pth'))
+if os.path.exists('./colornet_params_20_5.pth'):
+    color_model.load_state_dict(torch.load('colornet_params_20_5.pth'))
 if have_cuda:
     color_model.cuda()
 optimizer = optim.Adadelta(color_model.parameters())
@@ -41,6 +41,7 @@ def train(epoch):
     color_model.train()
     print('Epoch',epoch)
     l2_loss = nn.MSELoss()
+    l1_loss = nn.L1Loss()
 
     try:
         for batch_idx, (data, classes) in enumerate(train_loader):
@@ -57,7 +58,7 @@ def train(epoch):
             optimizer.zero_grad()
             class_output, output = color_model(original_img, original_img)
             # ems_loss = torch.pow((img_ab - output), 2).sum() / torch.from_numpy(np.array(list(output.size()))).prod()
-            ems_loss = l2_loss(output,img_ab)
+            ems_loss = l2_loss(output,img_ab)+l1_loss(output,img_ab)
 
             # cross_entropy_loss = 1/300 * F.cross_entropy(class_output, classes)
             loss = ems_loss # + cross_entropy_loss
@@ -69,7 +70,7 @@ def train(epoch):
             optimizer.step()
             if batch_idx % 1000 == 0:
                 message = 'Train Epoch:%d\tPercent:[%d/%d (%.0f%%)]\tLoss:%.9f\n' % (
-                    epoch, batch_idx * len(data), len(train_loader.dataset),
+                    epoch, batch_idx * len(data), len(train_loader),
                     100. * batch_idx / len(train_loader), loss.item())
                 messagefile.write(message)
                 torch.save(color_model.state_dict(), 'colornet_params.pth')
@@ -82,7 +83,7 @@ def train(epoch):
         logfile.write(traceback.format_exc())
         logfile.close()
     finally:
-        torch.save(color_model.state_dict(), 'colornet_params_20_5.pth')
+        torch.save(color_model.state_dict(), 'colornet_params_20_5_l1.pth')
 
 
 for epoch in range(1, epochs + 1):
