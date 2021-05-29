daf052e2783ca74e292b21f209776d586caf7808
AlexanderNevada <alexander.qi.eins@gmail.com>
2021-05-30 01:40:40 +0200

    val 13

--- val.py
+++ val.py
@@ -13,8 +13,8 @@ import matplotlib.pyplot as plt
  
 data_dir = '/cluster/scratch/qimaqi/data_5k/colorization_val/' # "../places205"  # '/cluster/scratch/qimaqi/data_5k/colorization_test/
 have_cuda = False #torch.cuda.is_available()
-checkpoint = '/cluster/scratch/qimaqi/colornet_scratch_28_5/12.pth'  #'./pretrain.pkl' #'/cluster/scratch/qimaqi/colornet_scratch_28_5_l2/1.pth' # './colornet_params_25_5_pretrain.pth'    #
-save_color_dir = '/cluster/scratch/qimaqi/data_5k/demo/12_val/'
+checkpoint = '/cluster/scratch/qimaqi/colornet_scratch_28_5/13.pth'  #'./pretrain.pkl' #'/cluster/scratch/qimaqi/colornet_scratch_28_5_l2/1.pth' # './colornet_params_25_5_pretrain.pth'    #
+save_color_dir = '/cluster/scratch/qimaqi/data_5k/demo/13_val/'
 
 try:
     os.mkdir(save_color_dir)
